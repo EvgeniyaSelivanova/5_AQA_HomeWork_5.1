cec71525c5d78f13cddd4d9eb544723892367752
Evgeniya Selivanova <kulbatskaya.evgenia@gmail.com>
2021-04-05 22:50:59 +0300

    Changed condition on notification

--- src/test/java/ru/netology/delivery/test/TestDelivery.java
+++ src/test/java/ru/netology/delivery/test/TestDelivery.java
@@ -9,7 +9,7 @@ import ru.netology.delivery.data.DataGenerator;
 
 import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -42,13 +42,18 @@ public class TestDelivery {
         form.$("[data-test-id='phone'] input").setValue(validUser.getPhone());
         form.$("[data-test-id='agreement']").click();
         form.$$("button").find(exactText("Запланировать")).click();
-        $(withText("Успешно!")).shouldBe(Condition.visible);
+        String firstDate = $("[data-test-id='date'] input").getValue();
+        $("[data-test-id='success-notification'] .notification__content")
+                .should(exactText("Встреча успешно запланирована на " + firstDate)).shouldBe(visible);
         form.$("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         form.$("[data-test-id='date'] input").setValue(secondMeetingDate);
         form.$$("button").find(exactText("Запланировать")).click();
         $(withText("Необходимо подтверждение")).shouldBe(Condition.visible);
         $("[data-test-id='replan-notification'] button").click();
-        $(withText("Успешно!")).shouldBe(Condition.visible);
+        String secondDate = $("[data-test-id='date'] input").getValue();
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + secondDate));
+
     }
 
     @Test
